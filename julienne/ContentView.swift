b947d3ddcf245974a7cee2753fae51ca027fbac9
bmcmahen <ben.mcmahen@gmail.com>
2019-06-27 11:39:46 -0700

    Initial Commit

---
+++ julienne/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  julienne
+//
+//  Created by Ben McMahen on 2019-06-27.
+//  Copyright © 2019 Ben McMahen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
